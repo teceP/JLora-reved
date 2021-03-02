ae6368dfa1748df05e0bd335cf5d732ba5c06e96
Mario Teklic <mario.teklic77@googlemail.com>
2021-03-02 13:19:53 +0100

    .

--- src/main/java/de/teklic/mario/core/Initializer.java
+++ src/main/java/de/teklic/mario/core/Initializer.java
@@ -71,6 +71,10 @@ public class Initializer {
 
         logger.info("+++++ Config finished +++++");
 
+        //Register for update central
+        SerialPortInput.getInstance().addObserver(jLora);
+        UserInput.getInstance().addObserver(jLora);
+
         return jLoraModel;
     }
 
@@ -95,10 +99,6 @@ public class Initializer {
         //Write
         SerialPortOutput.getInstance().setPrintWriter(new PrintWriter(jLoraModel.getSerialPort().getOutputStream()));
         UserOutput.getInstance().setPrintStream(System.out);
-
-        //Register for update central
-        SerialPortInput.getInstance().addObserver(jLora);
-        UserInput.getInstance().addObserver(jLora);
 
         logger.info("Finished initializing read and write.");
     }
