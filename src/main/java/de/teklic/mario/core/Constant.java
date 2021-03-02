bb4a41d403d343ac47327636157ea05fb3bee751
Mario Teklic <mario.teklic77@googlemail.com>
2021-03-02 13:57:27 +0100

    .

--- src/main/java/de/teklic/mario/core/Constant.java
+++ src/main/java/de/teklic/mario/core/Constant.java
@@ -7,7 +7,7 @@ package de.teklic.mario.core;
 public interface Constant {
     String PORT = "/dev/ttyS0";
     String BROADCAST = "FFFF";
-    String CONFIG = "AT+CFG=433500000,5,9,7,1,1,0,0,0,0,3000,8,4";
+    String CONFIG = "AT+CFG=433500000,20,9,7,1,1,0,0,0,0,3000,8,4";
     String[] IRRELEVANTS = {"AT,OK", "AT,SENDED", "AT,SENDING", "T,OK", "ERR:CPU_BUSY", "AERR:CPU_BUSY"};
     long DEFAULT_TIMEOUT = 5000;
     int DEFAULT_RETRIES = 3;
