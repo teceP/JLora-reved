8059fb168b0c85f81db0d65b784cbd6c653444a8
Mario Teklic <mario.teklic77@googlemail.com>
2021-03-07 14:53:23 +0100

    - added filter solution for multi-received same messages

--- src/main/java/de/teklic/mario/io/input/Filterable.java
+++ src/main/java/de/teklic/mario/io/input/Filterable.java
@@ -5,6 +5,16 @@ package de.teklic.mario.io.input;
  */
 
 public interface Filterable {
+    /**
+     * If this string is redirected by any filter, the message wont be forwarded to the JLora class
+     */
     String SHOULD_NOT = "should_not";
+
+    /**
+     * A filter will be prepend before the new message will be forwarded to the JLora class
+     * @param msg Any string
+     * @return The same string if no action has be made,
+     * SHOULD_NOT final String if the String should not be forwarded because of any condition from the filter
+     */
     String filter(String msg);
 }
